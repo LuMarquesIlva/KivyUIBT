818286f8674e6d5e0f1520d16a0ddc99672f29b2
LuMarquesIlva <luismarques0504@proton.me>
2026-05-05 22:22:21 -0300

    Added support for GridLayout insertions on BoxLayout

--- core/interface.py
+++ core/interface.py
@@ -1,24 +1,52 @@
 from kivy.uix.button import Button
 from kivy.uix.layout import Layout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class Inter_Label(Label):
+
+    obj = None
+
+    def __init__(self, TEXT=str or None):
+        if TEXT is not None:
+            self.text = TEXT
+
+        self.obj = Label(text=TEXT, markup=True)
+    
+    def __call__(self):
+        return self.obj
+
+class Inter_Layout(Widget):
+    obj = None
+
+    def __init__(self):
+        self.obj = Widget()
+    
+    def __call__(self):
+        return self.obj
 
 # Button Class
 class Inter_Button(Button):
-    btn = None # Kivy Button Object
+    obj = None # Kivy Button Object
 
     def _SetButtonPosition_(self, x=int, y=int):
-        self.btn.pos = (x, y)
+        self.obj.pos = (x, y)
 
     def __init__(self, TEXT=str, PRESSFUNC=type(object) or None, POSITION=(int, int) or None):
-        self.btn = Button(text=TEXT, on_press=PRESSFUNC, pos=POSITION)
+        self.obj = Button(text=TEXT, on_press=PRESSFUNC, pos=POSITION)
 
     def __call__(self):
-        print(self.btn)
-        return self.btn
+        return self.obj
     
+
 class Screen(Layout):
 
     WidgetsList = []
+
+    def addLayout(self):
+        self.WidgetsList.append(Inter_Layout())
 
     # AddButton Function
     def addButton(self, TEXT=str, FUNC=type(object) or None, POSITION=(0, 0) or None):
@@ -26,38 +54,101 @@ class Screen(Layout):
 
     def renderWidgets(self):
         for x in self.WidgetsList:
-            print(x)
-            self.add_widget(x.btn)
+            self.add_widget(x.obj)
 
     def __init__(self, **kwargs):
         super(Screen, self).__init__(**kwargs)
 
-        print(self.WidgetsList)
-
     def __call__(self):
         self.renderWidgets()
         return self
+    
+
+    class Box(BoxLayout):
+
+        gridObj = None
+
+        WidgetsList = []
+
+        orientation = "vertical" # <vertical> and <horizontal>
+        spacing = 5
+
+        def addLabel(self, TEXT=str or None):
+            self.WidgetsList.append(Inter_Label(TEXT))
+
+        def getGrid(self):
+            try:
+                return self.WidgetsList[self.gridObj]
+            except:
+                raise Exception("This layout does not have a grid; Create with '<BoxLayoutObject>.addGridLayout()' first")
+
+        def addGridLayout(self):
+            gridTempObj = Screen.Grid()
+            self.WidgetsList.append(gridTempObj)
+            self.gridObj = self.WidgetsList.index(gridTempObj)
+            
+
+        def changeOrientation(self, ORIENTATION = str):
+            self.orientation = ORIENTATION
+
+        def addLayout(self):
+            self.WidgetsList.append(Inter_Layout())
+
+        # AddButton Function
+        def addButton(self, TEXT=str, FUNC=type(object) or None, POSITION=(0, 0) or None):
+            self.WidgetsList.append(Inter_Button(TEXT, FUNC, POSITION))
+        
+        def renderWidgets(self):
+            for x in self.WidgetsList:
+                self.add_widget(x.obj)
+
+
+        def __init__(self, ORIENTATION = str or None, SPACING = int or None, **kwargs):
+            super(Screen.Box, self).__init__(**kwargs)
+            
+            if ORIENTATION is not None:
+                self.orientation = ORIENTATION
+
+            if SPACING is not None:
+                self.spacing = SPACING
+
+        def __call__(self):
+            self.renderWidgets()
+            return self
+
 
     class Grid(GridLayout):
+
+        obj = None
 
         WidgetsList = []
 
         cols = 2
         rows = 2
 
+        def addLabel(self, TEXT=str or None):
+            self.WidgetsList.append(Inter_Label(TEXT))
+
+        def setRows(self, ROWS = int):
+            self.rows = ROWS
+        
+        def setCols(self, COLS = int):
+            self.cols = COLS
+
+        def addLayout(self):
+            self.WidgetsList.append(Inter_Layout())
+
         # AddButton Function
         def addButton(self, TEXT=str, FUNC=type(object) or None, POSITION=(0, 0) or None):
             self.WidgetsList.append(Inter_Button(TEXT, FUNC, POSITION))
-            print(self.WidgetsList)
         
         def renderWidgets(self):
             for x in self.WidgetsList:
-                print(x)
-                self.add_widget(x.btn)
+                self.add_widget(x.obj)
 
         def __init__(self, **kwargs):
             super(Screen.Grid, self).__init__(**kwargs)
-            print(self.WidgetsList)
+            self.obj = self
         
         def __call__(self):
             self.renderWidgets()
--- main.py
+++ main.py
@@ -11,9 +11,27 @@ from core.interface import Screen
 def Pri(instance):
     print("TEXTO")
 
-tela = Screen.Grid()
-tela.addButton("Teste 1", Pri, (50, 50))
-tela.addButton("TESTE 2", POSITION=(130, 300))
+tela = Screen.Box('horizontal', 100)
+tela.addGridLayout()
+TelaGrid = tela.getGrid()
+
+TelaGrid.setRows(3)
+TelaGrid.setCols(3)
+
+for x in range(6):
+    if x == 3:
+        TelaGrid.addLabel(f"[color=ff3333]TEST[/color] {x}")
+        print("YEAHHSH")
+        print(TelaGrid.WidgetsList)
+        continue
+    TelaGrid.addButton(f"TEST {x}")
+
+TelaGrid.renderWidgets()
+
+tela.addButton("Test 1", Pri, (50, 50))
+tela.addLabel("TEXTO")
+tela.changeOrientation('vertical')
+#tela.addButton("TESTE 2", POSITION=(130, 300))
 
 
 class Interface(App):
